c02b95d9309f3ec2cc97ebc11177ded8ba56cc67
asano <ryo-asano@akane.waseda.jp>
2020-06-08 10:19:26 +0900

    Add GroupedCollection

--- DataGridGrouping/ViewModel.cs
+++ DataGridGrouping/ViewModel.cs
@@ -12,6 +12,8 @@ namespace DataGridGrouping
     {
         public CollectionViewSource CollectionView { get; set; } = new CollectionViewSource();
         public ReactiveCollection<User> Users { get; set; } = new ReactiveCollection<User>();
+        //public ReactiveCollection<Grouping<User, Todo>> Todos { get; set; } = new ReactiveCollection<Grouping<User, Todo>>();
+        public GroupedCollection<User, Todo> Todos { get; set; } = new GroupedCollection<User, Todo>();
         public ViewModel()
         {
             // 初期データを作成
@@ -26,20 +28,30 @@ namespace DataGridGrouping
             Users.Add(user1);
             Users.Add(user2);
 
+            
+            //Todos.Add(new Grouping<User, Todo>(user1, user1.Todos));
+            //Todos.Add(new Grouping<User, Todo>(user2, user2.Todos));
+            Todos.Add(user1,changePassword);
+            Todos.Add(user2,changePassword);
             //CollectionViewSourceを作成
             CollectionView.IsSourceGrouped = true;
-            CollectionView.Source = Users;
-            CollectionView.ItemsPath = new Windows.UI.Xaml.PropertyPath("Todos");
+
+            //collectionBinding.SetB
+            CollectionView.Source = Todos;
+            //CollectionView.ItemsPath = new Windows.UI.Xaml.PropertyPath("Todos");
         }
         public void AddUser()
         {
             Todo changePassword = new Todo();
-            changePassword.Name.Value = "Change Password";
+            changePassword.Name.Value = "Change Password2";
+            
             changePassword.Content.Value = "change your password. init password is \"hack me\"";
 
             User user = new User();
             user.Todos.Add(changePassword);
             Users.Add(user);
+            Todos.Add(user,changePassword);
+            //Todos.Add(new Grouping<User, Todo>(user, user.Todos));
         }
     }
 }
